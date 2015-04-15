8e87058eb9835fc7ae5433e9eb57e299645fbca9
tolerance1 <tolerance1@users.noreply.github.com>
2015-04-15 16:53:03 +0300

    Changed the matrix representation from vector of vectors to a single vector
    Allocating one continuous chunk of memory only once makes more sense than making 1000 allocations of smaller memory chunks;
    Also, usage of a single vector has reduced the multiplication time of two 1000 x 1000 matrixes before transposition almost twice as compared to using a vector of vectors.

--- main.cpp
+++ main.cpp
@@ -9,6 +9,8 @@
 using namespace std;
 using namespace std::chrono;
 
+typedef vector<double> Matrix;
+
 class Data
 {
     public:
@@ -16,16 +18,12 @@ class Data
         system_clock::duration d1, d2, d3, d4, d5;
 };
 
-void resizeMatrix(vector<vector<double>>& matrix, size_t size)
+void resizeMatrix(Matrix& matrix, size_t size)
 {
-    for(size_t i = 0; i < size; ++i)
-    {
-        vector<double> row(size);
-        matrix.push_back(row);
-    }
+    matrix.resize(size * size);
 }
 
-void initMatrix(vector<vector<double>>& matrix, size_t size)
+void initMatrix(Matrix& matrix, size_t size)
 {
     static default_random_engine e; //generates unsigned random integers (default seed)
     static uniform_real_distribution<double> u(0, 1);//uniformly distributed from 0 to 1 exclusive
@@ -34,12 +32,12 @@ void initMatrix(vector<vector<double>>& matrix, size_t size)
     {
         for(size_t j = 0; j < size; ++j)//for every column
         {
-            matrix[i][j] = u(e);
+            matrix[i * size + j] = u(e);
         }
     }
 }
 
-void outputMatrix(ostream& output, string& name, vector<vector<double>>& matrix, size_t size)
+void outputMatrix(ostream& output, string& name, Matrix& matrix, size_t size)
 {
     output << name << endl;
 
@@ -47,15 +45,15 @@ void outputMatrix(ostream& output, string& name, vector<vector<double>>& matrix,
     {
         for(size_t j = 0; j < size; ++j)//for every column
         {
-            output << left << setw(7) << setprecision(3) << matrix[i][j];
+            output << left << setw(7) << setprecision(3) << matrix[i * size + j];
         }
         output << endl;
     }
     output << endl;
 }
 
-void matrixDot(vector<vector<double>>& matrix1, vector<vector<double>>& matrix2,
-               vector<vector<double>>& resultMatrix, size_t size)
+void matrixDot(Matrix& matrix1, Matrix& matrix2,
+               Matrix& resultMatrix, size_t size)
 {
     for(size_t i = 0; i < size; ++i)//for every row in resultMatrix
     {
@@ -64,14 +62,14 @@ void matrixDot(vector<vector<double>>& matrix1, vector<vector<double>>& matrix2,
             //dot M1 and M2 elements
             for(size_t k = 0; k < size; ++k)//'k' - number of elements in a M1 row/M2 column
             {
-                resultMatrix[i][j] = resultMatrix[i][j] + matrix1[i][k] * matrix2[k][j];
+                resultMatrix[i * size + j] += matrix1[i * size + k] * matrix2[k * size + j];
             }
         }
     }
 }
 
-void tranposedMatrixDot(vector<vector<double>>& matrix1, vector<vector<double>>& matrix2,
-                        vector<vector<double>>& resultMatrix, size_t size)
+void tranposedMatrixDot(Matrix& matrix1, Matrix& matrix2,
+                        Matrix& resultMatrix, size_t size)
 {
     for(size_t i = 0; i < size; ++i)//for every row in resultMatrix
     {
@@ -80,7 +78,7 @@ void tranposedMatrixDot(vector<vector<double>>& matrix1, vector<vector<double>>&
             //dot M1 and M2 elements
             for(size_t k = 0; k < size; ++k)//'k' - number of elements in a M1 row/M2 row
             {
-                resultMatrix[i][j] = resultMatrix[i][j] + matrix1[i][k] * matrix2[j][k];
+                resultMatrix[i * size + j] += matrix1[i * size + k] * matrix2[j * size + k];
             }
         }
     }
@@ -107,7 +105,7 @@ vector<size_t> readFromFile()
     return params;
 }
 
-void writeMatrix(string& name, vector<vector<double>>& matrix, size_t size)
+void writeMatrix(string& name, Matrix& matrix, size_t size)
 {
     ofstream output("results.txt", ios_base::app);
 
@@ -140,15 +138,15 @@ void writeData(Data& data)
     }
 }
 
-void transposeMatrix(vector<vector<double>>& matrix, size_t size)
+void transposeMatrix(Matrix& matrix, size_t size)
 {
     for(size_t i = 0; i < size; ++i)//for every row
     {
         for(size_t j = i + 1; j < size; ++j)//for every column
         {
-            double temp = matrix[i][j];
-            matrix[i][j] = matrix[j][i];
-            matrix[j][i] = temp;
+            double temp = matrix[i * size + j];
+            matrix[i * size + j] = matrix[j * size + i];
+            matrix[j * size + i] = temp;
         }
     }
 }
@@ -164,13 +162,12 @@ int main()
         data.size = *iter;
         cout << "Matrix size: "<< data.size << endl;
 
-        vector<vector<double>> M1, M2, RES, tM2, tRES;
+        Matrix M1, M2, RES, tM2, tRES;
         resizeMatrix(M1, data.size);
         tRES = tM2 = RES = M2 = M1;
 
-        typedef vector<vector<double>> VEC;
-        typedef pair<string, VEC&> PAIR;
-        vector<PAIR> pairs {{"M1", M1},
+        typedef pair<string, Matrix&> Pair;
+        vector<Pair> pairs {{"M1", M1},
                             {"M2", M2},
                             {"RES", RES},
                             {"tM2", tM2},
